0b98da8cffbaae9d7e5296d84965975268b6f676
alici <alici@DESKTOP-UNBR714>
2016-06-30 22:14:40 -0300

    Adicionado o Carregamento de Todos os links

--- JobCM/Program.cs
+++ JobCM/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading;
@@ -22,23 +24,28 @@ namespace JobCM
 
     public class ConstrutivoData
     {
-        public static string LogaConstrutivo(string strLogin, string strSenha)
+        public string StrSession { get; }
+
+        public bool Logado { get; }
+        public ConstrutivoData(string strLogin, string strSenha)
+        {
+            StrSession = LogaConstrutivo( strLogin, strSenha);
+
+            Logado = true;
+        }
+
+        private string LogaConstrutivo(string strLogin, string strSenha)
         {
             // Cria a requisição
-            RequestState hwState = new RequestState("http://jundiai.construtivo.com/ssf/s/portalLogin");
+            RequestState hwState = new RequestState("http://jundiai.construtivo.com/ssf/s/portalLogin")
+            {
+                StrBody = String.Format("j_username={0}&j_password={1}&okBtn=OK", strLogin, strSenha)
+            };
 
-            // Corpo da Solicitação
-            hwState.StrBody = String.Format("j_username={0}&j_password={1}&okBtn=OK", strLogin, strSenha);
-
-            // Seta para o Tipo POST
-            hwState.HwrRequest.Method = "POST";
-            hwState.HwrRequest.Host = "jundiai.construtivo.com";
-            hwState.HwrRequest.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+            // Carrega os cabeçalhos
+            hwState.HwrRequest.Referer = "http://jundiai.construtivo.com/ssf/a/do?p_name=ss_forum&p_action=1&action=__login";
             hwState.HwrRequest.Headers.Add("Accept-Language", "pt-BR,pt;q=0.8,en-US;q=0.5,en;q=0.3");
             hwState.HwrRequest.Headers.Add("Accept-Encoding", "gzip, deflate");
-            hwState.HwrRequest.Referer = "http://jundiai.construtivo.com/ssf/a/do?p_name=ss_forum&p_action=1&action=__login";
-            hwState.HwrRequest.ContentType = "application/x-www-form-urlencoded";
-            hwState.HwrRequest.KeepAlive = true;
 
             // Adiciona um Cookie Container
             hwState.HwrRequest.CookieContainer = new CookieContainer();
@@ -54,6 +61,43 @@ namespace JobCM
 
             // Retorna o Valor da Sessão
             return hwState.HwrRequest.Headers[6];
+        }
+
+        // Carrega todos os links
+        public DataTable GetAllLinksNames(String hyperlink)
+        {
+            // Cria os indices
+            Int32 erros = 0;
+
+            // Cria a requisição
+            RequestState hwState = new RequestState(hyperlink);
+
+            // Carrega os cabeçalhos
+            hwState.HwrRequest.Headers.Add("Accept-Language", "pt-BR,pt;q=0.8,en-US;q=0.5,en;q=0.3");
+            hwState.HwrRequest.Headers.Add("Accept-Encoding", "gzip, deflate");
+
+            // Adiciona um Cookie Container
+            hwState.HwrRequest.CookieContainer = new CookieContainer();
+
+            // Seta o cookie de saida            
+            hwState.HwrRequest.CookieContainer.SetCookies(new Uri("http://jundiai.construtivo.com"), StrSession);
+
+            do
+            {
+                // Começa a requisição assincronamente
+                hwState.HwrRequest.BeginGetRequestStream(new AsyncCallback(Requisicoes.GetRequestStreamCallback), hwState);
+
+                // Aguarda o temino da requisição
+                hwState.AllDone.WaitOne();
+
+                // Se houve erro incrementa o contador
+                if (hwState.BErro) erros++;
+
+            } while ((hwState.BErro) && (erros < 5));
+            
+
+            // Retorna a resposta
+            return HtmlProcess.LinkNamesFromHtml(hwState.OResposta as string);
         }
     }
 
@@ -86,7 +130,13 @@ namespace JobCM
         public RequestState(string strUrl)
         {
             // Cria a Requisição
-            HwrRequest = (HttpWebRequest)WebRequest.Create(strUrl);
+            HwrRequest = (HttpWebRequest) WebRequest.Create(strUrl);
+
+            HwrRequest.Method = "POST";
+            HwrRequest.Host = "jundiai.construtivo.com";
+            HwrRequest.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+            HwrRequest.ContentType = "application/x-www-form-urlencoded";
+            HwrRequest.KeepAlive = true;
 
             // Seta o AllDone para aguardar
             AllDone = new ManualResetEvent(false);
@@ -101,7 +151,54 @@ namespace JobCM
 
     class HtmlProcess
     {
-        
+        public static DataTable LinkNamesFromHtml(string strResposta)
+        {
+            // Carrega as Listas
+            List<String> links = GetLinksFromHtml(strResposta).ToList<String>();
+            List<String> titulos = GetNamesFromHtml(strResposta).ToList<String>();
+
+            // Cria a tabela
+            DataTable retorno = new DataTable();
+
+            // Adiciona as colunas
+            retorno.Columns.Add("Titulo");
+            retorno.Columns.Add("Links");
+
+            int indice;
+            int total = links.Count;
+
+            // Ajusta os links
+            for (indice = 0; indice < total; indice++)
+            {
+                object[] obj = { titulos[indice], links[indice] };
+                retorno.Rows.Add(obj);
+            }
+
+            return retorno;
+        }
+
+        // Carrega todos os links de uma página
+        private static String[] GetLinksFromHtml(String strHtml)
+        {
+            HtmlAgilityPack.HtmlDocument htmLdoc = new HtmlAgilityPack.HtmlDocument();
+            htmLdoc.LoadHtml(strHtml);
+
+            var temp = from no in htmLdoc.DocumentNode.Descendants("a") select no.GetAttributeValue("href", "");
+
+            return temp.ToArray();
+        }
+
+
+        // Carrega todos os links de uma página
+        private static String[] GetNamesFromHtml(String strHtml)
+        {
+            HtmlAgilityPack.HtmlDocument htmLdoc = new HtmlAgilityPack.HtmlDocument();
+            htmLdoc.LoadHtml(strHtml);
+
+            var temp = from no in htmLdoc.DocumentNode.Descendants("a") select no.InnerHtml.Replace("\n", "");
+
+            return temp.ToArray();
+        }
     }
 
     class AnaliseDiagramas
@@ -127,36 +224,65 @@ namespace JobCM
             // Cria a requisição
             RequestState hwState = (RequestState)callbackResult.AsyncState;
 
-            // Cria o fluxo de postagem
-            Stream postStream = hwState.HwrRequest.EndGetRequestStream(callbackResult);
+            if (hwState.StrBody != null)
+            {
+                // Cria o fluxo de postagem
+                Stream postStream = hwState.HwrRequest.EndGetRequestStream(callbackResult);
 
-            // Transforma em Bytes o corpo da requisição
-            byte[] byteArray = Encoding.UTF8.GetBytes(hwState.StrBody);
+                // Transforma em Bytes o corpo da requisição
+                byte[] byteArray = Encoding.UTF8.GetBytes(hwState.StrBody);
 
-            // Adiciona o corpo da requisição
-            postStream.Write(byteArray, 0, byteArray.Length);
+                // Adiciona o corpo da requisição
+                postStream.Write(byteArray, 0, byteArray.Length);
 
-            // fecha o fluxo
-            postStream.Close();
+                // fecha o fluxo
+                postStream.Close();
+            }
 
-            // Chava a resposta de modo assincrono
-            hwState.HwrRequest.BeginGetResponse(GetResponseStreamCallback, hwState);
+            // Chama a resposta de modo assincrono
+            try
+            {
+                hwState.HwrRequest.BeginGetResponse(new AsyncCallback(GetResponseStreamCallback), hwState);
+            }
+            catch
+            {
+                // Seta o Erro
+                hwState.BErro = true;
+
+                // Seta o AllDone
+                hwState.AllDone.Set();
+            }
         }
 
+        
         // Retorna Resposta de Solicitação
         static void GetResponseStreamCallback(IAsyncResult callbackResult)
         {
             // Carrega a String Temporária de Resposta
             RequestState hwState = (RequestState)callbackResult.AsyncState;
 
-            // Cria as requisições
-            hwState.HwResponse = (HttpWebResponse)hwState.HwrRequest.EndGetResponse(callbackResult);
-
-            // Usando a resposta Salva o Valor em uma String
-            using (StreamReader httpWebStreamReader = new StreamReader(hwState.HwResponse.GetResponseStream()))
+            try
             {
-                //Transforma o fluxo em String
-                hwState.OResposta = httpWebStreamReader.ReadToEnd();
+                // Cria as requisições
+                hwState.HwResponse = (HttpWebResponse)hwState.HwrRequest.EndGetResponse(callbackResult);
+
+                // Usando a resposta Salva o Valor em uma String
+                using (StreamReader httpWebStreamReader = new StreamReader(hwState.HwResponse.GetResponseStream()))
+                {
+                    //Transforma o fluxo em String
+                    hwState.OResposta = httpWebStreamReader.ReadToEnd();
+
+                    // Requisicao completada OK
+                    hwState.BErro = false;
+
+                    // Seta o All Done
+                    hwState.AllDone.Set();
+                }
+            }
+            catch
+            {
+                // Erro da Requisicao
+                hwState.BErro = true;
 
                 // Seta o All Done
                 hwState.AllDone.Set();
